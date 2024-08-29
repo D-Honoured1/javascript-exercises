9b5d2f9a2465bc5474306e91cdc83719918769a2
Daniel <jamesalvez347@gmail.com>
2024-08-29 21:27:54 +0100

    Updated

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(greet, time) {
+    if (time < 0) {
+        return "WRONG!";
+    }
 
+    let string =""
+
+    for (i=0; i<time; i++) {
+        string += greet;
+    }
+    return string;
 };
 
 // Do not edit below this line
