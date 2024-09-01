d00481da478f738ea996197202adaf51e6684333
Daniel <jamesalvez347@gmail.com>
2024-09-01 19:10:26 +0100

    Updated; easy

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const divisibleByFourHundred = year % 400 === 0;
+    
+    if (divisibleByFour && (!isCentury || divisibleByFourHundred)){
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
