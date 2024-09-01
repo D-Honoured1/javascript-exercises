24e7629d80de86ccdd5418ce65fc72d56e49676f
Daniel <jamesalvez347@gmail.com>
2024-09-01 17:56:31 +0100

    Updated

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+/*const removeFromArray = function(array, ...args) {
+    const newArray = [];
+
+    array.forEach(element => {
+        if (!args.includes(element)) {
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
+};*/
+
+const removeFromArray = function (array, ...args) {
+    return array.filter(val => !args.includes(val))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
